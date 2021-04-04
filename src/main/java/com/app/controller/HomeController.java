25a24df21f819985230f1c3ef33d7649e4085ea3
Prabhjyot15 <bamrahprabhjyot@gmail.com>
2021-04-04 20:11:02 +0530

    Updating get endpoint

--- src/main/java/com/app/controller/HomeController.java
+++ src/main/java/com/app/controller/HomeController.java
@@ -1,30 +1,24 @@
 package com.app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.app.model.MenuItem;
+import com.app.repository.MenuRepository;
 
 @RestController
 @CrossOrigin(origins="http://localhost:4200")  
 public class HomeController {
 
+	@Autowired
+	MenuRepository menuRepository;
+	
 	@GetMapping("/")
-	public String getHome() {
-		return "[{\r\n"
-				+ "    \"name\":\"Pav Bhaji\",\r\n"
-				+ "    \"price\":\"120\"\r\n"
-				+ "    },{\r\n"
-				+ "    \"name\":\"Noodles\",\r\n"
-				+ "    \"price\":\"180\"\r\n"
-				+ "    },{\r\n"
-				+ "    \"name\":\"Bhaji Rice\",\r\n"
-				+ "    \"price\":\"80\"\r\n"
-				+ "    },{\r\n"
-				+ "    \"name\":\"Aloo gobi\",\r\n"
-				+ "    \"price\":\"120\"\r\n"
-				+ "    },{\r\n"
-				+ "    \"name\":\"abcdeh\",\r\n"
-				+ "    \"price\":\"520\"\r\n"
-				+ "}]";
+	public List<MenuItem> getHome() {
+		return menuRepository.findAll();
 	}
 }
